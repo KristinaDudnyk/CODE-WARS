4415049644268a2bdc8d4f0b7fa9d4475260109a
Kristina Dudnyk <kristina.dudnyk20@gmail.com>
2023-02-23 18:54:25 +0000

    Beginner Series #2 Clock

---
+++ kata8-Beginner Series #2 Clock.js
@@ -0,0 +1,14 @@
+function past(h, m, s) {
+  let H = h * 60 * 60 * 1000;
+  let M = m * 60 * 1000;
+  let S = s * 1000;
+  return H + M + S;
+  //#Happy Coding! ^_^
+}
+function past(h, m, s) {
+  let convH = h * 60 * 60 * 1000;
+  let convM = m * 60 * 1000;
+  let convS = s * 1000;
+  return convH + convM + convS;
+  //#Happy Coding! ^_^
+}
