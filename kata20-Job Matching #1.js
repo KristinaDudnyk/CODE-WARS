8f765b6f8a6f77e261908925398040bc77060499
Kristina <kristina.dudnyk20@gmail.com>
2023-03-31 11:49:11 +0100

    Job Matching #1

---
+++ kata20-Job Matching #1.js
@@ -0,0 +1,7 @@
+function match(candidate, job) {
+  if(!candidate.minSalary || !job.maxSalary ){
+    throw "error"}
+     return (candidate.minSalary*0.9) <= job.maxSalary
+  
+  // is this job a valid match for the candidate?
+}
